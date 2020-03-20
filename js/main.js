1ba99427e98c1a9cc9852e3c852ef4595b9efd9a
Jasmin-Mediani <molihua1992@gmail.com>
2020-03-20 11:55:38 +0100

    aggiunte serie tv

--- js/main.js
+++ js/main.js
@@ -1,16 +1,188 @@
 $(document).ready(function () {
 
-    var apiUrlBase = "https://api.themoviedb.org/3";
+
+
+    ///////////////// RICERCA DI FILM /////////////////
 
     var source = $("#template-hdb-film").html();
     var templateSchedaFilm = Handlebars.compile(source);
 
 
     //quando clicco sul bottone, creo una variabile (testoDigitato) che ha il valore digitato nella barra di ricerca. 
-    $("#bottone-ricerca").click(function (event) {
+
+    $("#bottone-ricerca").click(function () {
+        cercaFilm();
+        $("#barra-ricerca").val("");
+    });
+
+
+    $("#barra-ricerca").keypress(function () {
+        if (event.key == "Enter") {
+            cercaFilm();
+        }
+    });
+
+
+
+
+    ///////////////// RICERCA DI SERIE TV ////////////////
+
+    $("#bottone-ricerca").click(function () {
+        cercaSerieTv();
+        $("#barra-ricerca").val("");
+    });
+
+
+    $("#barra-ricerca").keypress(function () {
+        if (event.key == "Enter") {
+            cercaSerieTv();
+        }
+    });
+
+
+
+
+
+    ///////////////// FUNZIONI //////////////////////
+
+    function cercaSerieTv() {
         var testoDigitato = $("#barra-ricerca").val().toLowerCase();
+        var apiUrlBase = "https://api.themoviedb.org/3";
+        $.ajax({
+
+            url: apiUrlBase + '/search/tv',
+            data: {
+                api_key: "bf6da085b921b2474703466befcb39eb",
+                query: testoDigitato,
+                language: "it-IT"
+            },
+            method: "GET",
+            success: function (infoInArrivo) {
+                var films = infoInArrivo.results;
+                $(".contenitore-titoli").children(".card").remove(); // così resta in vista solo la serie/saga appena cercata
+
+                for (var i = 0; i < films.length; i++) {
+                    var film = films[i];
+
+                    var stellePiene = Math.ceil((film.vote_average) / 2) // MEMO: è un numero (arrotondato per eccesso)
+                    var infoVisibiliFilm = {
+                        titolo: film.name,
+                        titoloOriginale: film.original_name,
+                        voto: film.vote_average,
+                        lingua: /*film.original_language,*/ "",
+                        filmId: film.id,
+                        copertina: "https://image.tmdb.org/t/p/w185" + film.poster_path, //aggiunto w185 per width immagine
+                        stella1: "far fa-star",
+                        stella2: "far fa-star",
+                        stella3: "far fa-star",
+                        stella4: "far fa-star",
+                        stella5: "far fa-star",
+
+                        stelle: stellePiene,
+                    }
 
 
+
+                    //////////////// BANDIERE IN BASE ALLA LINGUA ORIGINALE ////////////////
+
+                    switch (film.original_language) {
+                        case "en":
+                            infoVisibiliFilm.lingua = "/bandiere/united-kingdom-flag-icon-64.png";
+                            break;
+                        case "it":
+                            infoVisibiliFilm.lingua = "/bandiere/italy-flag-icon-64.png";
+                            break;
+                        case "fr":
+                            infoVisibiliFilm.lingua = "/bandiere/france-flag-icon-64.png";
+                            break;
+                        case "ja":
+                            infoVisibiliFilm.lingua = "/bandiere/japan-flag-icon-64.png";
+                            break;
+                        case "de":
+                            infoVisibiliFilm.lingua = "/bandiere/germany-flag-icon-64.png";
+                            break;
+
+                        case "zh":
+                            infoVisibiliFilm.lingua = "/bandiere/china-flag-icon-64.png";
+                            break;
+
+                        case "es":
+                            infoVisibiliFilm.lingua = "/bandiere/spain-flag-icon-64.png";
+                            break;
+
+                        case "ru":
+                            infoVisibiliFilm.lingua = "/bandiere/russia-flag-icon-64.png";
+                            break;
+
+
+                        default: film.original_language
+                    }
+
+                    /////////////////////// STELLE IN BASE AL VOTO /////////////////////
+
+                    if (stellePiene > 0)
+                        infoVisibiliFilm.stella1 = "fas fa-star";
+                    if (stellePiene > 1)
+                        infoVisibiliFilm.stella2 = "fas fa-star";
+                    if (stellePiene > 2)
+                        infoVisibiliFilm.stella3 = "fas fa-star";
+                    if (stellePiene > 3)
+                        infoVisibiliFilm.stella4 = "fas fa-star";
+                    if (stellePiene > 4)
+                        infoVisibiliFilm.stella5 = "fas fa-star";
+
+
+
+                    var templateSchedaFilmCompilata = templateSchedaFilm(infoVisibiliFilm);
+                    $(".contenitore-titoli").append(templateSchedaFilmCompilata);
+
+
+
+                    ///// INFO A COMPARSA SULLA CARD ////////
+
+
+                    $(".card").mouseenter(function () {
+                        $(".card").find("h4").hide();
+                        $(".card").find(".lingua").hide();
+                        $(".card").find("h3").hide();
+                        $(".card").find(".contenitore-stelle").hide();
+
+
+                        $(this).find("h4").show();
+                        $(this).find("h3").show();
+                        $(this).find(".lingua").show();
+                        $(this).find(".contenitore-stelle").show();
+
+
+                    });
+
+                    $(".card").mouseleave(function () {
+                        $(".card").find("h4").hide();
+                        $(".card").find("h3").hide();
+                        $(".card").find(".lingua").hide();
+                        $(".card").find(".contenitore-stelle").hide();
+
+
+                    });
+
+                    $(".card").find(".contenitore-stelle").hide(); //così compare solo con l'hover sulla card
+
+
+                }
+
+
+            },
+            error: function (errore) {
+                alert("C\' è un rrore nel caricamento della pagina");
+            }
+
+        });
+
+    }
+
+    function cercaFilm() {
+        var testoDigitato = $("#barra-ricerca").val().toLowerCase();
+        var apiUrlBase = "https://api.themoviedb.org/3";
         $.ajax({
 
             url: apiUrlBase + '/search/movie',
@@ -84,35 +256,6 @@ $(document).ready(function () {
 
                     /////////////////////// STELLE IN BASE AL VOTO /////////////////////
 
-                    /*
-                    if (stellePiene == 1) {
-                        infoVisibiliFilm.stella1 = "fas fa-star";
-                    } else if (stellePiene == 2) {
-                        infoVisibiliFilm.stella1 = "fas fa-star";
-                        infoVisibiliFilm.stella2 = "fas fa-star";
-
-                    } else if (stellePiene == 3) {
-                        infoVisibiliFilm.stella1 = "fas fa-star";
-                        infoVisibiliFilm.stella2 = "fas fa-star";
-                        infoVisibiliFilm.stella3 = "fas fa-star";
-
-                    } else if (stellePiene == 4) {
-                        infoVisibiliFilm.stella1 = "fas fa-star";
-                        infoVisibiliFilm.stella2 = "fas fa-star";
-                        infoVisibiliFilm.stella3 = "fas fa-star";
-                        infoVisibiliFilm.stella4 = "fas fa-star";
-
-                    } else if (stellePiene == 5) {
-                        infoVisibiliFilm.stella1 = "fas fa-star";
-                        infoVisibiliFilm.stella2 = "fas fa-star";
-                        infoVisibiliFilm.stella3 = "fas fa-star";
-                        infoVisibiliFilm.stella4 = "fas fa-star";
-                        infoVisibiliFilm.stella5 = "fas fa-star";
-                    }
-                    */
-
-
-
                     if (stellePiene > 0)
                         infoVisibiliFilm.stella1 = "fas fa-star";
                     if (stellePiene > 1)
@@ -160,11 +303,6 @@ $(document).ready(function () {
 
                     $(".card").find(".contenitore-stelle").hide(); //così compare solo con l'hover sulla card
 
-
-
-
-                    var copertina = infoVisibiliFilm.copertina;
-
                 }
 
 
@@ -175,8 +313,8 @@ $(document).ready(function () {
 
         });
 
+    }
 
-    });
 
 
 });
